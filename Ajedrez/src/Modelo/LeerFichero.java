d2bf4ed99fdc0484c41a011f7220cba74557da62
amadeo <amadeo@mbp-de-amadeo.lan>
2020-11-12 13:58:48 +0100

    Funciona todo

--- Ajedrez/src/Modelo/LeerFichero.java
+++ Ajedrez/src/Modelo/LeerFichero.java
@@ -7,37 +7,59 @@ import java.io.*;
  *
  * @author amadeo
  */
-class LeeFichero {
-   public static void main(String [] arg) {
+public class LeerFichero 
+{
+    public LeerFichero() throws IOException
+    {
       File archivo = null;
       FileReader fr = null;
       BufferedReader br = null;
+      
+      String nombreJug, apellidosJug;
 
-      try {
-         // Apertura del fichero y creacion de BufferedReader para poder
-         // hacer una lectura comoda (disponer del metodo readLine()).
-         archivo = new File ("./src/");
+      try 
+      {
+         archivo = new File ("./src/fichero.txt");
          fr = new FileReader (archivo);
          br = new BufferedReader(fr);
 
-         // Lectura del fichero
          String linea;
          while((linea=br.readLine())!=null)
-            System.out.println(linea);
-      }
-      catch(Exception e){
-         e.printStackTrace();
-      }finally{
-         // En el finally cerramos el fichero, para asegurarnos
-         // que se cierra tanto si todo va bien como si salta 
-         // una excepcion.
-         try{                    
-            if( null != fr ){   
-               fr.close();     
-            }                  
-         }catch (Exception e2){ 
-            e2.printStackTrace();
+         {
+             //Leer hasta una coma
+            if(br.readLine() == ",")
+            {
+                linea = br.readLine();  
+            }
+            
+            
+            //Leer hasta el final de linea
+            else if(br.readLine() == "\n")
+            {
+                linea = br.readLine();
+            }
+            
+            linea = "";
          }
       }
-   }
-}
+      catch(Exception e)
+      {
+         e.printStackTrace();
+      }
+      
+      finally
+      {
+         try
+        {                    
+            if( null != fr )   
+               fr.close();          
+        }
+         
+        catch (Exception e2)
+        { 
+            e2.printStackTrace();
+        }
+      }
+    }
+      
+  }
